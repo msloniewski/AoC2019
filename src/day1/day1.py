2e218890e4c194a37bfd5fcef79f3ec9982a1e74
msloniewski <marcin.sloniewski@gmail.com>
2019-12-01 10:44:02 +0100

    day1: part 2 done

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -9,21 +9,40 @@ class Day1(DayBase):
         super(Day1, self).__init__()
         self._masses = None
 
+    @staticmethod
+    def _process_fuel_consumption(mass):
+        fuel_needed = 0
+        while mass > 0:
+            mass = max(0, int(mass / 3) - 2)
+            fuel_needed = fuel_needed + mass
+        return fuel_needed
+
     def process_input(self):
         self._masses = self._file.readlines()
         for i, mass in enumerate(self._masses):
             self._masses[i] = int(self._masses[i])
 
-    def solve(self):
+    def solve1(self):
         self.process_input()
 
         fuel_needed = 0
         for mass in self._masses:
             fuel_needed = fuel_needed + max(0, int(mass / 3) - 2)
 
-        print(fuel_needed)
+        print(f"part1: {fuel_needed}")
+
+    def solve2(self):
+        self.process_input()
+
+        fuel_needed = 0
+        for mass in self._masses:
+            fuel_needed = fuel_needed + self._process_fuel_consumption(mass)
+
+        print(f"part2: {fuel_needed}")
 
 
 if __name__ == "__main__":
-    day = Day1()
-    day.solve()
+    dayPart1 = Day1()
+    dayPart1.solve1()
+    dayPart2 = Day1()
+    dayPart2.solve2()
