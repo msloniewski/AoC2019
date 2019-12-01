c55d572f1b2ffa4015be946789da3d10d2f2ce87
msloniewski <marcin.sloniewski@gmail.com>
2019-12-01 10:33:52 +0100

    day1: part 1 done

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -5,9 +5,25 @@ from common.DayBase import DayBase
 
 class Day1(DayBase):
 
+    def __init__(self):
+        super(Day1, self).__init__()
+        self._masses = None
+
+    def process_input(self):
+        self._masses = self._file.readlines()
+        for i, mass in enumerate(self._masses):
+            self._masses[i] = int(self._masses[i])
+
     def solve(self):
-        pass
+        self.process_input()
+
+        fuel_needed = 0
+        for mass in self._masses:
+            fuel_needed = fuel_needed + max(0, int(mass / 3) - 2)
+
+        print(fuel_needed)
 
 
 if __name__ == "__main__":
     day = Day1()
+    day.solve()
