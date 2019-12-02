859951e12a65bb8acc3f1f9661885a730080839f
msloniewski <marcin.sloniewski@gmail.com>
2019-12-02 07:05:21 +0100

    day2: part 2 done

--- src/day2/day2.py
+++ src/day2/day2.py
@@ -8,18 +8,19 @@ class Day2(DayBase):
     def __init__(self):
         super(Day2, self).__init__()
         self._instructions = None
+        self._instructions_backup = None
 
     def process_input(self):
         self._instructions = self._file.readline()
         self._instructions = re.split(",", self._instructions)
         for i, instr in enumerate(self._instructions):
             self._instructions[i] = int(self._instructions[i])
+        self._instructions_backup = self._instructions
 
-    def solve1(self):
-        self.process_input()
+    def _compute_program(self, noun, verb):
 
-        self._instructions[1] = 12
-        self._instructions[2] = 2
+        self._instructions[1] = noun
+        self._instructions[2] = verb
 
         i = 0
 
@@ -37,11 +38,28 @@ class Day2(DayBase):
                     self._instructions[destination] = operand1 * operand2
             i = i + 4
 
+    def solve1(self):
+        self.process_input()
+
+        self._compute_program(12, 2)
+
         print(f"part1: {self._instructions[0]}")
 
     def solve2(self):
-        pass
-        #print(f"part2: {fuel_needed}")
+        self.process_input()
+
+        noun = None
+        verb = None
+
+        for i in range(0, 100):
+            for j in range(0, 100):
+                self._instructions = self._instructions_backup[:]
+                self._compute_program(i, j)
+                if self._instructions[0] == 19690720:
+                    noun = i
+                    verb = j
+
+        print(f"part2: {100 * noun + verb}")
 
 
 if __name__ == "__main__":
