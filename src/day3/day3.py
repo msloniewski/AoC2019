80fecb11360e8ff37127b9dff0e14de9b7cfe76e
msloniewski <marcin.sloniewski@gmail.com>
2019-12-03 21:27:24 +0100

    day3: part 2 done

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -7,12 +7,15 @@ class Day3(DayBase):
 
     def __init__(self):
         super(Day3, self).__init__()
-        self._path_a = None
+        self._dirs_a = None
+        self._dirs_b = None
         self._path_b = None
+        self._path_a = None
+        self._intersections = None
 
     def process_input(self):
-        self._path_a = re.split(',', self._file.readline())
-        self._path_b = re.split(',', self._file.readline())
+        self._dirs_a = re.split(',', self._file.readline())
+        self._dirs_b = re.split(',', self._file.readline())
 
     @staticmethod
     def _compute_part_of_path(part, current_coord):
@@ -43,27 +46,44 @@ class Day3(DayBase):
     def solve1(self):
         self.process_input()
 
-        intersections = []
+        self._intersections = []
 
-        path_a = ['0,0']
-        for direction in self._path_a:
-            path_a = path_a + self._compute_part_of_path(direction, path_a[-1])
+        self._path_a = ['0,0']
+        for direction in self._dirs_a:
+            self._path_a = self._path_a + self._compute_part_of_path(direction, self._path_a[-1])
 
-        path_b = ['0,0']
-        for direction in self._path_b:
-            path_b = path_b + self._compute_part_of_path(direction, path_b[-1])
+        self._path_b = ['0,0']
+        for direction in self._dirs_b:
+            self._path_b = self._path_b + self._compute_part_of_path(direction, self._path_b[-1])
 
-        for point in path_a[1:]:
-            if point in path_b:
-                intersections.append(point)
+        for point in self._path_a[1:]:
+            if point in self._path_b:
+                self._intersections.append(point)
 
-        closest_intersection = min(intersections, key=self.manhattan_norm)
+        closest_intersection = min(self._intersections, key=self.manhattan_norm)
         print(f"part1: {self.manhattan_norm(closest_intersection)}")
 
     def solve2(self):
-        self.process_input()
+        self.solve1()
+
+        path_a_distances = {}
+        path_b_distances = {}
+        sum_of_distances = []
+
+        for i, point in enumerate(self._path_a):
+            if point in self._intersections:
+                if point not in path_a_distances:
+                    path_a_distances[point] = i
+
+        for i, point in enumerate(self._path_b):
+            if point in self._intersections:
+                if point not in path_b_distances:
+                    path_b_distances[point] = i
+
+        for point in path_a_distances:
+            sum_of_distances.append(path_a_distances[point] + path_b_distances[point])
 	
-        print(f"part2: {0}")
+        print(f"part2: {min(sum_of_distances)}")
 
 
 if __name__ == "__main__":
